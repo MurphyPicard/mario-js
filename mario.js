160ef573daeb056a20ae89778de829530e0d69da
murphypicard <ayaghsizian@gmail.com>
2017-03-12 17:21:30 -0400

    mario 3 works ARA

--- mario.js
+++ mario.js
@@ -12,20 +12,25 @@ function determineHeightAndThenDrawPyramid() {
 
     // TODO 3
     // figure out the height the user typed (replace the "5" below)
-    heightStr = "5";
+
+
+    heightStr = document.getElementById('height').value;
 
     // here we convert the string to an int
     height = parseInt(heightStr);
-
+    console.log(height);
     // TODO 2
     // draw the pyramid with the given height
-
+    drawPyramid(height);
 }
 
 
 // TODO 1
 // hook up the button's click event to our determineHeightAndThenDrawPyramid function
-
+var start = document.querySelector('button');
+start.onclick = () => {
+  determineHeightAndThenDrawPyramid();
+};
 
 
 /**
@@ -36,7 +41,7 @@ function determineHeightAndThenDrawPyramid() {
  function drawPyramid(height) {
 
      // TODO 4
-     // before drawing, clear the old content
+     document.getElementById('pyramid').innerHTML = "";
 
 
      // for each row....
